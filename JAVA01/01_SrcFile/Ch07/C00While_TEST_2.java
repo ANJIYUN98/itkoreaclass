d8daad19725ca1d54c077b13aa7717e41b4f2ab9
ANJIYUN98 <155033351+ANJIYUN98@users.noreply.github.com>
2024-02-29 13:40:14 +0900

    Add files via upload

--- JAVA01/01_SrcFile/Ch07/C00While_TEST_2.java
+++ JAVA01/01_SrcFile/Ch07/C00While_TEST_2.java
@@ -46,29 +46,129 @@ public class C00While_TEST_2 {
 		//구구단 출력(2단 - N단)
 		//N은 Scanner로 받습니다.
 		
-		Scanner sc = new Scanner(System.in);
+//		Scanner sc = new Scanner(System.in);
+//		
+//		int n = sc.nextInt();
+//		while(n>9) {
+//		
+//			System.out.println("다시입력하세요.");
+//			n = sc.nextInt();
+//		}
+//		
+//		int dan=2;
+//		int i;
+//		
+//		
+//		while(dan<=n) {
+//			i=1;
+//			
+//			while(i<=9) {
+//				System.out.printf("%d x %d = %d\n", dan,i,dan*i);
+//				i++;
+//			}
+//			System.out.println();
+//			dan++;
+//		}
 		
-		int n = sc.nextInt();
-		while(n>9) {
 		
-			System.out.println("다시입력하세요.");
-			n = sc.nextInt();
-		}
-		
-		int dan=2;
-		int i;
+		//4행 별찍기 -> 높이 입력 별찍기
+		//****
+		//****
+		//****
+		//****
 		
 		
-		while(dan<=n) {
-			i=1;
+//		int i=0; //행증가
+//		int j=0; //별찍기
+//		while(i<4) {
+//			
+//			j=0;
+//			while(j<4) {
+//				System.out.print("*");
+//				j++;
+//			}
+//			
+//			System.out.println();
+//			i++;
+//		}
+		
+		
+		
+		//4행 별찍기 -> 높이 입력받아 별찍기
+		//높이 :n
+		//****
+		//****
+	    //****
+	    //****
+		
+//		높이 : n
+//		행증가(i)		별찍기(j)
+//		0		0~3
+//		1		0~3
+//		2		0~3
+//		3		0~3
+//		..
+//		n-1		0~3
+//		------------------------------------------
+//		i=0		j=0
+//		i<n		j<4
+//		i++		j++
+		
+		
+		//정사각형 만들려면 j<4를 j<n으로 바꿔주면 됨
+				
+//		Scanner sc = new Scanner(System.in);
+//		int n = sc.nextInt();
+//		int i=0; //행증가
+//		int j=0; //별찍기
+//		while(i<n) {
+//					
+//			j=0;
+//			while(j<4) {
+//				System.out.print("*");
+//				j++;
+//			}
+//					
+//			System.out.println();
+//			i++;
+//		}
+		
+		
+		
+		//*
+		//**
+		//***
+		//****
+		
+//		i		j
+//		0		0-0
+//		1		0-1
+//		2		0-2
+//		3		0-3
+//		----------------------------
+//		i=0		j=0
+//		i<4		j<=i  j는 i의 수보다 작다가 똑같아지므로
+//		i++		j++
+		
+		
+		int i=0; //행증가
+		int j=0; //별찍기
+		while(i<4) {
 			
-			while(i<=9) {
-				System.out.printf("%d x %d = %d\n", dan,i,dan*i);
-				i++;
+			j=0;
+			while(j<=i) {
+				System.out.print("*");
+				j++;
 			}
+			
 			System.out.println();
-			dan++;
+			i++;
 		}
+		
+		
+		
+		
+		
 		
 	}
 
